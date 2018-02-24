ea118c115a94ba4b2c1f9cb20beca0e8b155e169
Marcin Kumorek <kumeek@gmail.com>
2018-02-24 20:14:57 +0100

    Add post Mutation

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
 
 const resolvers = {
 	Query,
+	Mutation
 }
 
 const server = new GraphQLServer({
@@ -15,9 +17,9 @@ const server = new GraphQLServer({
 			typeDefs: 'src/generated/prisma.graphql',
 			endpoint: 'https://eu1.prisma.sh/public-peridotglazer-665/hackernews-node/dev', // the endpoint of the Prisma DB service
 			secret: 'mysecret123', // specified in database/prisma.yml
-			debug: true, // log all GraphQL queryies & mutations
-		}),
-	}),
+			debug: true // log all GraphQL queryies & mutations
+		})
+	})
 })
 
 server.start(() => console.log('Server is running on http://localhost:4000'))
---
+++ src/resolvers/Mutation.js
@@ -0,0 +1,7 @@
+const post = function(parent, args, context, info) {
+	const { url, description } = args
+
+	return context.db.mutation.createLink({ data: { url, description } }, info)
+}
+
+module.exports = { post }
