b25ff9f5eb5cbe5911c42da94b3739aaf9719c92
Marcin Kumorek <kumeek@gmail.com>
2018-02-24 21:39:14 +0100

    Add signup Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,7 +1,22 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const APP_SECRET = require('../utils')
+
 const post = function(parent, args, context, info) {
 	const { url, description } = args
 
 	return context.db.mutation.createLink({ data: { url, description } }, info)
 }
 
-module.exports = { post }
+// eslint-disable-next-line
+const signup = async function signup(parent, args, context, info) {
+	const password = await bcrypt.hash(args.password, 10)
+	const user = await context.db.mutation.createUser({
+		data: { ...args, password }
+	})
+	const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+	return { token, user }
+}
+
+module.exports = { post, signup }
