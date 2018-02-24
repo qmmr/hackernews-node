c8667cfdbabaaf9bb42bdd666ea7348ae774558b
Marcin Kumorek <kumeek@gmail.com>
2018-02-24 21:53:55 +0100

    Add login Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -19,4 +19,23 @@ const signup = async function signup(parent, args, context, info) {
 	return { token, user }
 }
 
-module.exports = { post, signup }
+const login = async function login(parent, args, context, info) {
+	const user = await context.db.query.user({ where: { email: args.email } })
+	if (!user) {
+		throw new Error(`Could not find user with email: ${args.email}`)
+	}
+
+	const valid = await bcrypt.compare(args.password, user.password)
+	if (!valid) {
+		throw new Error('Invalid password')
+	}
+
+	const token = jwt.sign({ userId: user.id }, APP_SECRET)
+
+	return {
+		token,
+		user
+	}
+}
+
+module.exports = { post, signup, login }
