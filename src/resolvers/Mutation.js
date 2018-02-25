6b884bb624616ebb8b04d0fee81f4150a5a1f4c2
Marcin Kumorek <kumeek@gmail.com>
2018-02-25 13:48:35 +0100

    Add vote resolver Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -46,4 +46,27 @@ const login = async function login(parent, args, context, info) {
 	}
 }
 
-module.exports = { post, signup, login }
+const vote = async function vote(parent, args, context, info) {
+	const userId = getUserId(context)
+	const { linkId } = args
+	const linkExists = await context.db.exists.Vote({
+		user: { id: userId },
+		link: { id: linkId }
+	})
+
+	if (linkExists) {
+		throw new Error(`Already voted for link: ${linkId}`)
+	}
+
+	return context.db.mutation.createVote(
+		{
+			data: {
+				user: { connect: { id: userId } },
+				link: { connect: { id: linkId } }
+			}
+		},
+		info
+	)
+}
+
+module.exports = { post, signup, login, vote }
