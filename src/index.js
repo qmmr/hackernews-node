cff87dbc4aa93bd0ea973024e019a508dd04583e
Marcin Kumorek <kumeek@gmail.com>
2018-02-25 20:30:18 +0100

    Add Subscription resolvers and add to index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const { GraphQLServer } = require('graphql-yoga')
 const { Prisma } = require('prisma-binding')
 const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
+const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
 	Query,
-	Mutation
+	Mutation,
+	Subscription
 }
 
 const server = new GraphQLServer({
